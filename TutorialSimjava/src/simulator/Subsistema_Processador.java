e61ddf63016769f5c9ead055a8e14a7e0d406b52
Johanny LS <johanny.dls@mail.com>
2018-06-13 19:16:49 -0300

    Criada classe de simulação. Ainda não está gerando o sim_trace. Vou ver qual o problema ainda, mas está funcional

---
+++ TutorialSimjava/src/simulator/Subsistema_Processador.java
@@ -0,0 +1,33 @@
+package simulator;
+
+import eduni.simjava.Sim_system;
+
+public class Subsistema_Processador {
+	
+	public static void main(String[] args) {
+		
+		//Inicializa o Sim_system
+		Sim_system.initialise();
+		
+		//Adiciona a Fonte
+		Fonte fonte = new Fonte("Fonte", 50);
+		
+		//Adiciona o processador
+		Processador processador = new Processador("Processador", 30);
+		
+		//Adiciona o disco 1
+		Disco disco1 = new Disco("Disco1", 60);
+		
+		//Adiciona o disco 2
+		Disco disco2 = new Disco("Disco2", 110);
+		
+		//Liga as portas das entidades
+		Sim_system.link_ports("Fonte", "Out", "Processador", "In");
+		Sim_system.link_ports("Processador", "Out1", "Disco1", "In");
+		Sim_system.link_ports("Processador", "Out2", "Disco2", "In");
+		
+		//Executa a simulação
+		Sim_system.run();
+	}
+
+}
